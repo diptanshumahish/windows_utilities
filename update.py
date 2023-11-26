08f3ba4e5782d0dd30d3783d66b5316fd8663b96
diptanshumahish <diptanshumahish2016@gmail.com>
2023-11-26 12:35:43 +0530

    update to designs

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 import subprocess
+from tqdm import tqdm as tqdm_progress
+from colorama import init, Fore, Style
 
 # Check if colorama is installed, and install it if necessary
 try:
@@ -17,6 +19,7 @@ except ImportError:
     subprocess.run(["pip", "install", "tqdm"])
 
 # Initialize colorama for colored output
+init()
 
 def run_command(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -36,20 +39,23 @@ def copy_files():
     destination_dir = script_dir
 
     print(f"{Fore.GREEN}Copying new files...{Style.RESET_ALL}")
-    for root, _, files in os.walk(source_dir):
-        for file in tqdm.tqdm(files, unit='file', desc="Copying"):
-            source_path = os.path.join(root, file)
-            destination_path = os.path.join(destination_dir, file)
-            shutil.copy2(source_path, destination_path)
+    total_files = sum(len(files) for _, _, files in os.walk(source_dir))
+    
+    with tqdm_progress(total=total_files, unit='file', desc="Copying") as pbar:
+        for root, _, files in os.walk(source_dir):
+            for file in files:
+                source_path = os.path.join(root, file)
+                destination_path = os.path.join(destination_dir, file)
+                shutil.copy2(source_path, destination_path)
+                pbar.update(1)
+
+    print(f"\n{Fore.GREEN}Copy complete!{Style.RESET_ALL}")
 
 def main():
-    # Update repository
     update_repository()
-
-    # Copy new files
     copy_files()
 
-    print(f"\n{Fore.GREEN}Update complete! {Fore.WHITE}Thank you for using this tool!{Style.RESET_ALL}")
+    print(f"\n{Fore.GREEN}Update complete! \n{Fore.WHITE}Thank you for using this tool!{Style.RESET_ALL}")
 
 if __name__ == "__main__":
     main()
